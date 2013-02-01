14aaff75f490ddfd0495609aa9cecaf9a6ade2af
Mike McNeil <michael.r.mcneil@gmail.com>
2013-02-01 13:04:35 -0600

    Extrapolated criteria handling into separate file.

---
+++ criteria.js
@@ -0,0 +1,186 @@
+// Find models in data which satisfy the options criteria, 
+// then return their indices in order
+module.exports = function getMatchIndices(data, options) {
+	// Remember original indices
+	var origIndexKey = '_waterline_dirty_origindex';
+	var matches = _.clone(data);
+	_.each(matches, function(model, index) {
+		// Determine origIndex key
+		// while (model[origIndexKey]) { origIndexKey = '_' + origIndexKey; }
+		model[origIndexKey] = index;
+	});
+
+	// Query and return result set using criteria
+	matches = applyFilter(matches, options.where);
+	matches = applySort(matches, options.sort);
+	matches = applySkip(matches, options.skip);
+	matches = applyLimit(matches, options.limit);
+	var matchIndices = _.pluck(matches, origIndexKey);
+
+	// Remove original index key which is keeping track of the index in the unsorted data
+	_.each(data, function(datum) {
+		delete datum[origIndexKey];
+	});
+	return matchIndices;
+};
+
+// Run criteria query against data aset
+function applyFilter(data, criteria) {
+	if(!data) return data;
+	else {
+		return _.filter(data, function(model) {
+			return matchSet(model, criteria);
+		});
+	}
+}
+
+function applySort(data, sort) {
+	if(!sort || !data) return data;
+	else {
+		var sortedData = _.clone(data);
+
+		// Sort through each sort attribute criteria
+		_.each(sort, function(direction, attrName) {
+
+			var comparator;
+
+			// Basic MongoDB-style numeric sort direction
+			if(direction === 1 || direction === -1) comparator = function(model) {
+				return model[attrName];
+			};
+			else comparator = comparator;
+
+			// Actually sort data
+			sortedData = _.sortBy(sortedData, comparator);
+
+
+			// Reverse it if necessary (if -1 direction specified)
+			if(direction === -1) sortedData.reverse();
+		});
+		return sortedData;
+	}
+}
+
+// Ignore the first *skip* models
+function applySkip(data, skip) {
+	if(!skip || !data) return data;
+	else {
+		return _.rest(data, skip);
+	}
+}
+
+function applyLimit(data, limit) {
+	if(!limit || !data) return data;
+	else {
+		return _.first(data, limit);
+	}
+}
+
+
+// Match a model against each criterion in a criteria query
+
+function matchSet(model, criteria) {
+
+	// Null or {} WHERE query always matches everything
+	if(!criteria || _.isEqual(criteria, {})) return true;
+
+	// By default, treat entries as AND
+	return _.all(criteria, function(criterion, key) {
+		return matchItem(model, key, criterion);
+	});
+}
+
+
+function matchOr(model, disjuncts) {
+	var outcome = false;
+	_.each(disjuncts, function(criteria) {
+		if(matchSet(model, criteria)) outcome = true;
+	});
+	return outcome;
+}
+
+function matchAnd(model, conjuncts) {
+	var outcome = true;
+	_.each(conjuncts, function(criteria) {
+		if(!matchSet(model, criteria)) outcome = false;
+	});
+	return outcome;
+}
+
+function matchLike(model, criteria) {
+	for(var key in criteria) {
+
+		var matchString = criteria[key];
+
+		if(_.isRegExp(matchString)) {
+			// awesome
+		} else if(_.isString(matchString)) {
+			// Handle escaped percent (%) signs
+			matchString = matchString.replace(/%%%/g, '%');
+
+			// Replace SQL % match notation with something the ECMA regex parser can handle
+			matchString = matchString.replace(/([^%]*)%([^%]*)/g, '$1.*$2');
+
+			// Case insensitive by default
+			var modifiers = 'i';
+			// TODO: make this overridable
+			matchString = new RegExp('^' + matchString + '$', modifiers);
+		}
+		// Unexpected match string!
+		else {
+			console.error('matchString:');
+			console.error(matchString);
+			throw new Error("Unexpected match string: " + matchString + " Please use a regexp or string.");
+		}
+
+		// Deal with non-strings by creating index
+		var index = model[key];
+		if(_.isNumber(index)) index = "" + index;
+		else if(_.isBoolean(index)) index = index ? "true" : "false";
+		else if(!_.isString(index)) {
+			// Ignore objects, arrays, null, and undefined data for now
+			// (and maybe forever)
+			return false;
+		}
+
+		// Check that criterion attribute and is at least similar to the model's value for that attr
+		if(!index.match(matchString)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function matchNot(model, criteria) {
+	return !matchSet(model, criteria);
+}
+
+function matchItem(model, key, criterion) {
+
+	if(key.toLowerCase() === 'or') {
+		return matchOr(model, criterion);
+	} else if(key.toLowerCase() === 'not') {
+		return matchNot(model, criterion);
+	} else if(key.toLowerCase() === 'and') {
+		return matchAnd(model, criterion);
+	} else if(key.toLowerCase() === 'like') {
+		return matchLike(model, criterion);
+	}
+	// IN query
+	else if(_.isArray(criterion)) {
+		return _.any(criterion, function(val) {
+			return model[key] === val;
+		});
+	}
+	// ensure the key attr exists in model
+	else if(_.isUndefined(model[key])) {
+		return false;
+	}
+
+	// ensure the key attr matches model attr in model
+	else if((model[key] !== criterion)) {
+		return false;
+	}
+	// Otherwise this is a match
+	return true;
+}
