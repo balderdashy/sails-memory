ccf21e593e236b31a1ad14fadeff6fbf318d7415
Mike McNeil <michael.r.mcneil@gmail.com>
2013-02-09 23:09:33 -0600

    Support for new waterline semantics (0.4.x)

--- DirtyAdapter.js
+++ DirtyAdapter.js
@@ -30,89 +30,94 @@ function DirtyAdapter () {
 	var uuid = require('node-uuid');
 
 
+	// Maintain connections to files and memory collection
+	var connections = {	};
+
+	// String to precede key name for schema defininitions
+	var schemaPrefix = 'waterline_schema_';
+
+	// String to precede key name for actual data in collection
+	var dataPrefix = 'waterline_data_';
+
+
 	var adapter = {
 
-		config: {
+		// Enable transactions by allowing Dirty to create a commitLog
+		commitLog: true,
+
+		// Default configuration for collections
+		defaults: {
 
 			// If inMemory is true, all data will be destroyed when the server stops
 			// otherwise, it will be written to disk
 			inMemory: true,
 
 			// File path for disk file output (when NOT in inMemory mode)
-			dbFilePath: './.waterline/dirty.db',
-
-			// String to precede key name for schema defininitions
-			schemaPrefix: 'waterline_schema_',
-
-			// String to precede key name for actual data in collection
-			dataPrefix: 'waterline_data_'
+			filePath: './.waterline/dirty.db'
 		},
 
-		// Initialize the underlying data model
-		initialize: function(cb) {
-			var my = this;
+		// Logic to handle the (re)instantiation of collections
+		registerCollection: function(collection, cb) {
+			var self = this;
+			var collectionName = collection.identity;
 
-			if(! this.config.inMemory) {
-				// Check that dbFilePath file exists and build tree as neessary
-				require('fs-extra').touch(this.config.dbFilePath, function(err) {
-					if(err) return cb(err);
-					my.db = new(dirty.Dirty)(my.config.dbFilePath);
+			// If the configuration in this collection corresponds 
+			// with an existing connection, reuse it
+			connections[collection.identity] = _.find(connections, function (connection, name) {
+				return connection.inMemory === collection.inMemory &&
+						connection.filePath === collection.filePath;
+			});
 
+			if (connections[collection.identity]) afterwards();
+
+			// Otherwise initialize for the first time
+			else {
+				connect(collection, function (err, connection) {
+					// Save reference to connection
+					connections[collection.identity] = connection;
 					afterwards();
 				});
-			} else {
-				this.db = new(dirty.Dirty)();
-				afterwards();
 			}
 
 			function afterwards() {
 
-				// Trigger callback with no error
-				my.db.on('load', function() {
-					cb();
-				});
-			}
-		},
+				// Grab current auto-increment value from database and populate it in-memory
+				// (it's ok if it doesn't exist-- it will only exist if Dirty persisting to disk 
+				// and the collection has been initialized at least once)
+				var schema = connections[collectionName].db.get(schemaPrefix + collectionName);
 
-		// Logic to handle the (re)instantiation of collections
-		initializeCollection: function(collectionName, cb) {
-			var self = this;
+				statusDb[collectionName] = (schema && schema.autoIncrement) ? schema : {autoIncrement: 1};
 
-			// Grab current auto-increment value from database and populate it in-memory
-			// (it's ok if it doesn't exist-- it will only exist if Dirty persisting to disk 
-			// and the collection has been initialized at least once)
-			var schema = this.db.get(this.config.schemaPrefix + collectionName);
+				self.getAutoIncrementAttribute(collectionName, function (err,aiAttr) {
+					// Check that the resurrected auto-increment value is valid
+					self.find(collectionName, {
+						where: {
+							id: statusDb[collectionName].autoIncrement
+						}
+					}, function (err, models) {
+						if (err) return cb(err);
 
-			statusDb[collectionName] = (schema && schema.autoIncrement) ? schema : {autoIncrement: 1};
+						// If that model already exists, warn the user and generate the next-best possible auto-increment key
+						if (models && models.length) {
 
-			self.getAutoIncrementAttribute(collectionName, function (err,aiAttr) {
-				// Check that the resurrected auto-increment value is valid
-				self.find(collectionName, {
-					where: {
-						id: statusDb[collectionName].autoIncrement
-					}
-				}, function (err, models) {
-					if (err) return cb(err);
+							// Find max
+							self.find(collectionName, {}, function (err,models) {
+								var autoIncrement = _.max(models,function (model){
+									return model[aiAttr];
+								});
+								autoIncrement = autoIncrement && autoIncrement[aiAttr] || 0;
+								autoIncrement++;
 
-					// If that model already exists, warn the user and generate the next-best possible auto-increment key
-					if (models && models.length) {
-
-						// Find max
-						self.find(collectionName, {}, function (err,models) {
-							var autoIncrement = _.max(models,function (model){
-								return model[aiAttr];
+								// self.log.warn("On-disk auto-increment ID corrupt, using: "+autoIncrement+" on attribute:",aiAttr);
+								statusDb[collectionName].autoIncrement = autoIncrement;
+								cb();
 							});
-							autoIncrement = autoIncrement && autoIncrement[aiAttr] || 0;
-							autoIncrement++;
-
-							self.log.warn("On-disk auto-increment ID corrupt, using: "+autoIncrement+" on attribute:",aiAttr);
-							statusDb[collectionName].autoIncrement = autoIncrement;
-							cb();
-						});
-					}
-					else cb();
+						}
+						else cb();
+					});
 				});
-			});
+
+			}
 		},
 
 		// Logic to handle flushing collection data to disk before the adapter shuts down
@@ -121,16 +126,16 @@ function DirtyAdapter () {
 			
 			// Always go ahead and write the new auto-increment to disc, even though it will be wrong sometimes
 			// (this is done so that the auto-increment counter can be "resurrected" when the adapter is restarted from disk)
-			var schema = this.db.get(this.config.schemaPrefix + collectionName);
-			if (!schema) return cb(badSchemaError(collectionName, this.config.schemaPrefix));
+			var schema = connections[collectionName].db.get(schemaPrefix + collectionName);
+			if (!schema) return cb(badSchemaError(collectionName, schemaPrefix));
 
 			schema = _.extend(schema,{
 				autoIncrement: statusDb[collectionName].autoIncrement
 			});
 
-			this.log.verbose("Waterline saving "+collectionName+" collection...");
-			this.db.set(this.config.schemaPrefix + collectionName, schema, function (err) {
-				my.db = null;
+			// this.log.verbose("Waterline saving "+collectionName+" collection...");
+			connections[collectionName].db.set(schemaPrefix + collectionName, schema, function (err) {
+				connections[collectionName].db = null;
 				cb && cb(err);
 			});
 		},
@@ -139,11 +144,11 @@ function DirtyAdapter () {
 		// Fetch the schema for a collection
 		// (contains attributes and autoIncrement value)
 		describe: function(collectionName, cb) {	
-			this.log.verbose(" DESCRIBING :: " + collectionName);
+			// this.log.verbose(" DESCRIBING :: " + collectionName);
 			
 			// (it's ok if schema doesn't exist-- it will only exist if Dirty persisting to disk 
 			// and the collection has been initialized at least once)
-			var schema = this.db.get(this.config.schemaPrefix + collectionName);
+			var schema = connections[collectionName].db.get(schemaPrefix + collectionName);
 
 			var attributes = schema && schema.attributes;
 			return cb(null, attributes);
@@ -151,9 +156,9 @@ function DirtyAdapter () {
 
 		// Create a new collection
 		define: function(collectionName, attributes, cb) {
-			this.log.verbose(" DEFINING " + collectionName, {
-				as: schema
-			});
+			// this.log.verbose(" DEFINING " + collectionName, {
+			// 	as: schema
+			// });
 			var self = this;
 
 			var schema = {
@@ -162,7 +167,7 @@ function DirtyAdapter () {
 			};
 
 			// Write schema and status objects
-			return self.db.set(this.config.schemaPrefix + collectionName, schema, function(err) {
+			return connections[collectionName].db.set(schemaPrefix + collectionName, schema, function(err) {
 				if(err) return cb(err);
 
 				// Set in-memory schema for this collection
@@ -174,10 +179,10 @@ function DirtyAdapter () {
 		// Drop an existing collection
 		drop: function(collectionName, cb) {
 			var self = this;
-			self.log.verbose(" DROPPING " + collectionName);
-			return self.db.rm(self.config.dataPrefix + collectionName, function(err) {
+			// self.log.verbose(" DROPPING " + collectionName);
+			return connections[collectionName].db.rm(dataPrefix + collectionName, function(err) {
 				if(err) return cb("Could not drop collection!");
-				return self.db.rm(self.config.schemaPrefix + collectionName, cb);
+				return connections[collectionName].db.rm(schemaPrefix + collectionName, cb);
 			});
 		},
 
@@ -187,16 +192,16 @@ function DirtyAdapter () {
 
 		// Create one or more new models in the collection
 		create: function(collectionName, values, cb) {
-			this.log.verbose(" CREATING :: " + collectionName, values);
+			// this.log.verbose(" CREATING :: " + collectionName, values);
 			values = _.clone(values) || {};
-			var dataKey = this.config.dataPrefix + collectionName;
-			var data = this.db.get(dataKey);
+			var dataKey = dataPrefix + collectionName;
+			var data = connections[collectionName].db.get(dataKey);
 			var self = this;
 
 
 			// Lookup schema & status so we know all of the attribute names and the current auto-increment value
-			var schema = this.db.get(this.config.schemaPrefix + collectionName);
-			if (!schema) return cb(badSchemaError(collectionName, this.config.schemaPrefix));
+			var schema = connections[collectionName].db.get(schemaPrefix + collectionName);
+			if (!schema) return cb(badSchemaError(collectionName, schemaPrefix));
 
 			// Auto increment fields that need it
 			doAutoIncrement(collectionName, schema.attributes, values, this, function (err, values) {
@@ -211,7 +216,7 @@ function DirtyAdapter () {
 					data.push(values);
 
 					// Replace data collection and go back
-					self.db.set(dataKey, data, function(err) {
+					connections[collectionName].db.set(dataKey, data, function(err) {
 						return cb(err, values);
 					});
 				});
@@ -222,8 +227,8 @@ function DirtyAdapter () {
 		// using where, limit, skip, and order
 		// In where: handle `or`, `and`, and `like` queries
 		find: function(collectionName, options, cb) {
-			var dataKey = this.config.dataPrefix + collectionName;
-			var data = this.db.get(dataKey) || [];
+			var dataKey = dataPrefix + collectionName;
+			var data = connections[collectionName].db.get(dataKey) || [];
 
 			// Get indices from original data which match, in order
 			var matchIndices = getMatchIndices(data,options);
@@ -238,15 +243,15 @@ function DirtyAdapter () {
 
 		// Update one or more models in the collection
 		update: function(collectionName, options, values, cb) {
-			this.log.verbose(" UPDATING :: " + collectionName, {
-				options: options,
-				values: values
-			});
+			// this.log.verbose(" UPDATING :: " + collectionName, {
+			// 	options: options,
+			// 	values: values
+			// });
 			var my = this;
 
 			
-			var dataKey = this.config.dataPrefix + collectionName;
-			var data = this.db.get(dataKey);
+			var dataKey = dataPrefix + collectionName;
+			var data = connections[collectionName].db.get(dataKey);
 
 			// Query result set using options
 			var matchIndices = getMatchIndices(data,options);
@@ -263,17 +268,17 @@ function DirtyAdapter () {
 			});
 
 			// Replace data collection and go back
-			this.db.set(dataKey, data, function(err) {
+			connections[collectionName].db.set(dataKey, data, function(err) {
 				cb(err, resultSet);
 			});
 		},
 
 		// Delete one or more models from the collection
 		destroy: function(collectionName, options, cb) {
-			this.log.verbose(" DESTROYING :: " + collectionName, options);
+			// this.log.verbose(" DESTROYING :: " + collectionName, options);
 
-			var dataKey = this.config.dataPrefix + collectionName;
-			var data = this.db.get(dataKey);
+			var dataKey = dataPrefix + collectionName;
+			var data = connections[collectionName].db.get(dataKey);
 
 			// Query result set using options
 			var matchIndices = getMatchIndices(data,options);
@@ -286,7 +291,7 @@ function DirtyAdapter () {
 			});
 
 			// Replace data collection and respond with what's left
-			this.db.set(dataKey, data, function(err) {
+			connections[collectionName].db.set(dataKey, data, function(err) {
 				cb(err);
 			});
 		},
@@ -303,6 +308,41 @@ function DirtyAdapter () {
 	//////////////                 //////////////////////////////////////////
 	////////////// Private Methods //////////////////////////////////////////
 	//////////////                 //////////////////////////////////////////
+
+	// Initialize an underlying data connection given a collection def
+	function connect (collection, cb) {
+		var collectionName = collection.identity;
+
+		// Default to inMemory if no file path is specified
+		if (_.isUndefined(collection.inMemory) && !collection.filePath) {
+			collection.inMemory = true;
+		}
+
+		if( collection.inMemory ) {
+			var memDb = new(dirty.Dirty)();
+			memDb.on('load', function() { 
+				cb(null, {
+					db			: memDb,
+					inMemory	: true
+				}); 
+			});
+		}
+		else  {
+
+			// Check that filePath file exists and build tree as neessary
+			require('fs-extra').touch(collection.filePath, function(err) {
+				if(err) return cb(err);
+				
+				var fileDb = new(dirty.Dirty)(collection.filePath);
+				fileDb.on('load', function() { 
+					cb(null, {
+						db			: fileDb,
+						inMemory	: true
+					}); 
+				});
+			});
+		}
+	}
 
 	// Look for auto-increment field, increment counter accordingly, and return refined value set
 	function doAutoIncrement (collectionName, attributes, values, ctx, cb) {
@@ -332,16 +372,15 @@ function DirtyAdapter () {
 		return cb(null,values);
 	}
 
+
 	return adapter;
 }
 
 // Public API
-//	* NOTE: The public API for adapters is a function that can be passed a set of options
-//	* It returns the complete adapter, augmented with the options provided
-module.exports = function (options) {
-	var adapter = new DirtyAdapter();
-	adapter.config = _.extend(adapter.config, options || {});
-	return adapter;
+//	* NOTE: The public API for adapters is a function 
+//	* It returns the instantiated adapter
+module.exports = function () {
+	return new DirtyAdapter();
 };
 
 function badSchemaError(collectionName, schemaPrefix) {
